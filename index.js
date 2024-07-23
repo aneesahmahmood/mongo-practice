545f5e78b292765a4713a1119486d12636f814b3
Aneesah Mahmood <aneesah.mahmood1@gmail.com>
2024-07-23 16:11:09 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+
+// let app = express();
+
+// app.get("/hello", (req, res) => {
+//   res.send("hello world");
+// });
+
+// app.listen(3000, () => {
+//   console.log("server running on port 3000");
+// });
